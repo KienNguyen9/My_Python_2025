30cd677e6f4c2113177198e1fdf550265c900fcb
KienNguyen9 <kierannguyen98@gmail.com>
2024-12-29 00:05:29 +0700

    Update some code experiment about abs- path

--- App_1_Todo_List/code_experiments/code_experiment_1.py
+++ App_1_Todo_List/code_experiments/code_experiment_1.py
@@ -1,8 +1,22 @@
-contents = ["How to aaa", "learn", "Python"]
-file_names = ["doc.txt", "report.txt", "presentation.txt"]
+import sys
+import os
 
-for content, file_name in zip(contents, file_names):
-    with open(f"./../output/{file_name}", "w") as file:
-        file.writelines(content)
+current_script_path = os.path.abspath(__file__)
+file_name = os.path.basename(__file__)
+current_folder_path = current_script_path.split(file_name)[0].replace("\\","/")
 
-print("Done!")
+def write_to_files():
+    contents = ["How to aaa", "learn", "Python"]
+    file_names = ["doc.txt", "report.txt", "presentation.txt"]
+
+    for content, file_name in zip(contents, file_names):
+        with    open(f"{current_folder_path}../output/{file_name}", "w") as file:
+            file.writelines(content)
+
+    a = "every things "\
+        "Done"
+    print(a)
+
+
+if __name__ == "__main__":
+    write_to_files()
---
+++ App_1_Todo_List/main copy.py
@@ -0,0 +1,3 @@
+from code_experiments import code_experiment_1 
+
+code_experiment_1.write_to_files()
