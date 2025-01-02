7091bd79a4827654c49216ae2b239234c17b3bea
KienNguyen9 <kierannguyen98@gmail.com>
2025-01-02 23:25:55 +0700

    Update list comprehension

--- App_1_Todo_List/code_experiments/code_experiment_jan_02/main.py
+++ App_1_Todo_List/code_experiments/code_experiment_jan_02/main.py
@@ -1,3 +1,9 @@
 filenames = ['1.doc', '1.report', '1.presentation']
 filenames = [filename.replace(".", "-") + ".txt" for filename in  filenames ]
 print(filenames)
+
+
+# multiples 2
+numbers = [10, 20, 30]
+numbers_multiplies = [number*2 for number in numbers]
+print(numbers_multiplies)
