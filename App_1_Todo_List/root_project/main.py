52cac04f337f47a3681924804f1c5882aef82081
KienNguyen9 <kierannguyen98@gmail.com>
2025-01-04 01:56:26 +0700

    Update 'completed' option

--- App_1_Todo_List/root_project/main.py
+++ App_1_Todo_List/root_project/main.py
@@ -4,7 +4,6 @@ Author: Kien Nguyen
 """
 import os
 
-
 def print_out_list_of_task(lines_list):
     print("--- List of existing task ---")
     for index, element in enumerate(lines_list):
@@ -17,7 +16,6 @@ output_folder_path = r"./output"
 # Create output if it doesn't exist.
 if not os.path.exists(output_folder_path):
     os.makedirs(output_folder_path)
-
 
 while True:
     user_action = input("Type add, show, edit, completed or exit: ").strip()
@@ -38,7 +36,6 @@ while True:
             
             with open(r"./output/file_todo_list.txt", "w", encoding = "utf-8    ") as file:
                 file.writelines(lines)              
-
         case "show":
             try: 
                 with open("./output/file_todo_list.txt","r") as file:
@@ -58,12 +55,20 @@ while True:
             with open("./output/file_todo_list.txt", "w") as file:
                 todo_list[num- 1] = (input("Enter new thing todo: ") + "\n")
                 file.writelines(todo_list)
-
         case "completed":
-            pass
+            # Show the list
+            with open("./output/file_todo_list.txt", "r") as file:
+                todo_list = file.readlines()
+                print_out_list_of_task(todo_list)
+            
+            done = int(input("Enter number of completed task: "))
+            todo_list.pop(done-1)
+
+            with open("./output/file_todo_list.txt", "w") as file:
+                file.writelines(todo_list)
+            
         case "exit":
             break
-            pass
 
 
 
