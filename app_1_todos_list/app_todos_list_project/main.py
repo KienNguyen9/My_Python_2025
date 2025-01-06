2a7a1d86b09361e6ef35cc64343868886cf636f1
KienNguyen9 <kierannguyen98@gmail.com>
2025-01-06 23:19:21 +0700

    Use if-else instead of match-case

--- app_1_todos_list/app_todos_list_project/main.py
+++ app_1_todos_list/app_todos_list_project/main.py
@@ -21,66 +21,69 @@ if not os.path.exists(output_folder_path):
     os.makedirs(output_folder_path)
 
 while True:
-    user_action = input("Type add, show, edit, completed or exit: ").strip()
-    match user_action:
-        case "add":
+    user_action = input("Type add, show, edit, completed or exit: ").lower().strip()
+
+    if "add" in user_action:
+        lines = []
+        
+        # When the file wasn't created yet
+        try:
+            with open(r"./output/file_todo_list.txt", "r", encoding = "utf-8    ") as file:
+                lines = file.readlines() 
+        except FileNotFoundError:
             lines = []
-                        
-            try:
-                with open(r"./output/file_todo_list.txt", "r", encoding = "utf-8    ") as file:
-                    lines = file.readlines() 
-            except FileNotFoundError:
-                lines = []
-            
-            print_out_list_of_task(lines)
+        
+        # Show the list of task        
+        # print_out_list_of_task(lines)
 
+        # new_task = input("Enter new task: ").capitalize().strip() + "\n"
+        if user_action.strip().lower() == 'add':
             new_task = input("Enter new task: ").capitalize().strip() + "\n"
-            lines.append(new_task)
-            
-            with open(r"./output/file_todo_list.txt", "w", encoding = "utf-8    ") as file:
-                file.writelines(lines)              
-        case "show":
-            try: 
-                with open("./output/file_todo_list.txt","r") as file:
-                    todo_list = file.readlines()
-                print_out_list_of_task(todo_list)
-            except:
-                print("Chua co du lieu!")
-        case "edit":
-            todo_list = []
-            with open("./output/file_todo_list.txt", "r") as file:
+        else:
+            new_task = user_action.replace('add', '').strip() + "\n" 
+        
+        # Add new task to the list    
+        lines.append(new_task)
+        
+        # Write to file
+        with open(r"./output/file_todo_list.txt", "w", encoding = "utf-8    ") as file:
+            file.writelines(lines)              
+
+    elif "show" in user_action:
+        try: 
+            with open("./output/file_todo_list.txt","r") as file:
                 todo_list = file.readlines()
-                for index , item in enumerate(todo_list):
-                    print("\t",index + 1, ". ", item.capitalize(), end = '')
+            print_out_list_of_task(todo_list)
+        except:
+            print("Chua co du lieu!")
 
-            num = int(input("select the number you want to edit: "))
-            
-            with open("./output/file_todo_list.txt", "w") as file:
-                todo_list[num- 1] = (input("Enter new thing todo: ") + "\n")
-                file.writelines(todo_list)
-        case "completed":
-            
+    elif "edit" in user_action:
+        todo_list = []
+        with open("./output/file_todo_list.txt", "r") as file:
+            todo_list = file.readlines()
+            for index , item in enumerate(todo_list):
+                print("\t",index + 1, ". ", item.capitalize(), end = '')
+        num = int(input("select the number you want to edit: "))
+        with open("./output/file_todo_list.txt", "w") as file:
+            todo_list[num- 1] = (input("Enter new thing todo: ") + "\n")
+            file.writelines(todo_list)
 
-            # Show the list
-            with open("./output/file_todo_list.txt", "r") as file:
-                todo_list = file.readlines()
-                if todo_list == []:
-                    print("--- There is nothing to do now ---")
-                    continue
-                print_out_list_of_task(todo_list)
-            
-            done = int(input("Enter number of completed task: "))
-            
-            thing_completed = todo_list.pop(done-1)[:-1]
+    elif "completed" in user_action:
+        with open("./output/file_todo_list.txt", "r") as file:
+            todo_list = file.readlines()
+            if todo_list == []:
+                print("--- There is nothing to do now ---")
+                continue
+            print_out_list_of_task(todo_list)
+        done = int(input("Enter number of completed task: "))
+        thing_completed = todo_list.pop(done-1)[:-1]
+        with open("./output/file_todo_list.txt", "w") as file:
+            file.writelines(todo_list)
+        message = f"The task '{thing_completed}' was done."
+        print(message)
 
-            with open("./output/file_todo_list.txt", "w") as file:
-                file.writelines(todo_list)
-
-            message = f"The task '{thing_completed}' was done."
-            print(message)
-
-        case "exit":
-            break
+    elif "exit" in user_action:
+        break
 
 
 
