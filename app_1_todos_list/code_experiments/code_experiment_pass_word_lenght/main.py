06cc994d0e12161446566e645b2b77761a30e1c8
KienNguyen9 <kierannguyen98@gmail.com>
2025-01-11 12:01:34 +0700

    Update side code experiment that  pass word checking

--- app_1_todos_list/code_experiments/code_experiment_pass_word_lenght/main.py
+++ app_1_todos_list/code_experiments/code_experiment_pass_word_lenght/main.py
@@ -1,3 +1,5 @@
+import re
+
 # code experiment 
 # "Strong Pass Word" checking
 prompt_s = "Strong pass word"
@@ -5,11 +7,28 @@ prompt_w = "Weak pass word"
 prompt_n = "Pass word is not valid"
 
 while True:
+    
+    result = []
     pass_word = input("Enter the pass word: ")
-    if pass_word:
-        pass
-    elif True:
-        pass
+    
+    # length checking
+    if len(pass_word) < 8:
+        result.append(False)
     else:
-        pass
+        result.append(True)
+    
+    # Number checking 
+    if any(char.isdigit() for char in pass_word):
+        result.append(True)
+    else:
+        result.append(False)
+            
+    
+    # Upper case checking
+    if any(char.isupper() for char in pass_word):
+        result.append(True)
+    else:
+        result.append(False)
+
+    print(result)
 
